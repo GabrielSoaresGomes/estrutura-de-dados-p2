1742c807013f039693f8b0c234a3fa3887cf89b2
Myze <mizaellucaspardal@hotmail.com>
2022-11-24 22:56:24 -0300

    BORAAAAAAAAAAA

--- src/layouts/Footer/Footer.js
+++ src/layouts/Footer/Footer.js
@@ -3,6 +3,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className='footer'>
+      <h1>Trabalho de Estrutura de Dados P2 - Fábio Gonçalves</h1>
       <h1>Gabriel Soares Gomes - 202110116</h1>
       <h1>Junior Nascimento Guimarães - 20211010323</h1>
       <h1>Mizael Lucas Pardal Gonçalves Ribeiro - 202120339</h1>
